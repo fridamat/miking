1b6b470ad71d9f32f611385df3c3c10f26990401
Alfrida <fridamat@kth.se>
2019-08-02 11:28:42 +0200

    New start of processing

--- src/boot/seqprocessing.ml
+++ src/boot/seqprocessing.ml
@@ -154,40 +154,53 @@ let upd_rels_assoc_list_list_entry key rels_assoc_l new_val =
     let upd_rels_assoc_l2 = List.remove_assoc key upd_rels_assoc_l1 in
     (key,new_val)::upd_rels_assoc_l2
 
-let rec transl_rels_to_rels_assoc_list rels rels_assoc_l =
+(*let rec transl_rels_to_rels_assoc_list rels rels_assoc_l =
   match rels with
   | [] -> rels_assoc_l
   | (hd1,hd2)::tl ->
     let upd_rels_assoc_l = upd_rels_assoc_list_list_entry hd2 rels_assoc_l hd1 in
-    transl_rels_to_rels_assoc_list tl upd_rels_assoc_l
+    transl_rels_to_rels_assoc_list tl upd_rels_assoc_l*)
+let rec transl_rels_to_rels_assoc_list2 rels rels_assoc_l =
+  match rels with
+  | [] -> rels_assoc_l
+  | (hd1,hd2)::tl ->
+    let upd_rels_assoc_l1 = upd_rels_assoc_list_list_entry hd1 rels_assoc_l hd2 in
+    let upd_rels_assoc_l2 = upd_rels_assoc_list_list_entry hd2 upd_rels_assoc_l1 hd1 in
+    transl_rels_to_rels_assoc_list2 tl upd_rels_assoc_l2
 
-let rec init_found_seqs_assoc_list rels_assoc_l =
+let rec init_visited_seqs_assoc_list rels_assoc_l =
   match rels_assoc_l with
   | [] -> []
   | (hd,_)::tl ->
-    (hd,false)::(init_found_seqs_assoc_list tl)
+    (hd,false)::(init_visited_seqs_assoc_list tl)
 
-let rec find_all_related seqs rels_assoc_l new_seqs rel_seqs found_seqs_assoc_l =
-  match seqs, new_seqs with
-  | [], [] -> (rel_seqs,found_seqs_assoc_l)
-  | [], _ -> find_all_related new_seqs rels_assoc_l [] rel_seqs found_seqs_assoc_l
+let rec find_all_related_seqs rels_assoc_l curr_seqs new_seqs visited_assoc_l all_seqs =
+  match curr_seqs, new_seqs with
+  | [], [] -> (all_seqs,visited_assoc_l)
+  | [], _ -> find_all_related_seqs rels_assoc_l new_seqs [] visited_assoc_l all_seqs
   | (hd::tl), _ ->
-    if List.assoc hd found_seqs_assoc_l then
-      find_all_related tl rels_assoc_l new_seqs rel_seqs found_seqs_assoc_l
+    if List.assoc hd visited_assoc_l then
+      find_all_related_seqs rels_assoc_l tl new_seqs visited_assoc_l all_seqs
     else
-      let upd_found_seqs_assoc_l = upd_rels_assoc_list_bool_entry hd found_seqs_assoc_l true in
+      (*Mark hd as visited*)
+      let upd_visited_assoc_l = upd_rels_assoc_list_bool_entry hd visited_assoc_l true in
       let hd_rel_seqs = List.assoc hd rels_assoc_l in
       let upd_new_seqs = List.append hd_rel_seqs new_seqs in
-      let upd_rel_seqs = hd::rel_seqs in
-      find_all_related tl rels_assoc_l upd_new_seqs upd_rel_seqs upd_found_seqs_assoc_l
+      let upd_all_seqs = hd::all_seqs in
+      find_all_related_seqs rels_assoc_l tl upd_new_seqs upd_visited_assoc_l upd_all_seqs
 
-let rec reduce_rels seq_cons rels_assoc_l red_rels_assoc_l found_seqs_assoc_l =
-  match seq_cons with
-  | [] -> rels_assoc_l
-  | hd::tl ->
-    let (rel_seqs,upd_found_seqs_assoc_l) = find_all_related [hd] rels_assoc_l [] [] found_seqs_assoc_l in
-    let upd_red_rels_assoc_l = (hd,rel_seqs)::red_rels_assoc_l in
-    reduce_rels tl rels_assoc_l upd_red_rels_assoc_l upd_found_seqs_assoc_l
+let rec reduce_rels rels_assoc_l visited_assoc_l =
+  match rels_assoc_l with
+  | [] -> []
+  | (hd,hdl)::tl ->
+    if List.assoc hd visited_assoc_l then
+      reduce_rels tl visited_assoc_l
+    else
+      (*Mark hd as visited*)
+      let upd_visited_assoc_l1 = upd_rels_assoc_list_bool_entry hd visited_assoc_l true in
+      (*Get all relatives of hd*)
+      let (hd_rel_seqs,upd_visited_assoc_l2) = find_all_related_seqs rels_assoc_l (List.assoc hd rels_assoc_l) [] visited_assoc_l [] in
+      (hd,hd_rel_seqs)::(reduce_rels rels_assoc_l upd_visited_assoc_l2)
 
 let process_ast ast =
   let _ = Printf.printf "The program is %s\n" (Ustring.to_utf8 (Pprint.pprint false ast)) in
@@ -202,10 +215,8 @@ let process_ast ast =
   let rels_assoc_l1 = init_rels_assoc_list seqs in
   let _ = Printf.printf "The first version of the rels assoc list:\n%s\n" (get_rels_assoc_list_string rels_assoc_l1) in
   (*Transfer relationships in rels to the rels assoc list*)
-  let rels_assoc_l2 = transl_rels_to_rels_assoc_list rels rels_assoc_l1 in
+  let rels_assoc_l2 = transl_rels_to_rels_assoc_list2 rels rels_assoc_l1 in
   let _ = Printf.printf "The second version of the rels assoc list:\n%s\n" (get_rels_assoc_list_string rels_assoc_l2) in
-  (*Reduce relationships*)
-  let rels_assoc_l3 = reduce_rels seq_cons rels_assoc_l2 [] (init_found_seqs_assoc_list rels_assoc_l2) in
-  (*Continue here since we don't get the right rels*)
+  let rels_assoc_l3 = reduce_rels rels_assoc_l2 (init_visited_seqs_assoc_list rels_assoc_l2) in
   let _ = Printf.printf "The third version of the rels assoc list:\n%s\n" (get_rels_assoc_list_string rels_assoc_l3) in
   ast
