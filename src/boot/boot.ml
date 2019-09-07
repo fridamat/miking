5ff5ee72d768b267919ac74113d09ef1fb2c52af
Alfrida <fridamat@kth.se>
2019-09-07 19:04:45 +0200

    Fix comparer methods

--- src/boot/boot.ml
+++ src/boot/boot.ml
@@ -21,6 +21,7 @@ open Dssa
 open Testseqprocessing
 open Queue
 open Realtimequeue
+open Comparers
 
 let prog_argv = ref []          (* Argv for the program that is executed *)
 
@@ -120,100 +121,15 @@ let rec debruijn env t =
                  Case(fi,pat,debruijn (patvars env pat) tm)) cases)
   | TmNop -> t
 
-let compare_tm_terms tm1 tm2 =
-  match tm1, tm2 with
-  | TmConst(_,CInt(n1)), TmConst(_,CInt(n2)) -> (n1 = n2)
-  | _ -> false
-
-let rec compare_linked_lists ll1 ll2 i =
-  if (i = Linkedlist.length ll1) && (i = Linkedlist.length ll2) then
-    true
-  else if (i = Linkedlist.length ll1) || (i = Linkedlist.length ll2) then
-    false
-  else if (compare_tm_terms (Linkedlist.nth ll1 i) (Linkedlist.nth ll2 i)) then
-    compare_linked_lists ll1 ll2 (i+1)
-  else
-    false
-
-let compare_sequences seq1 seq2 =
-  match seq1, seq2 with
-  | SeqList(ll1), SeqList(ll2) ->
-    Linkedlist.equals ll1 ll2
-  | SeqQueue(q1), SeqQueue(q2) ->
-    Queue.equals q1 q2
-  | SeqRTQueue(rtq1), SeqRTQueue(rtq2) ->
-    Realtimequeue.equals rtq1 rtq2
-  | SeqNone, SeqNone -> true
-  | _ -> failwith "Comparison of sequence type not implemented."
-
-let rec compare_int_lists l1 l2 =
-  match l1, l2 with
-  | [], [] -> true
-  | hd1::tl1, hd2::tl2 ->
-    if compare_tm_terms hd1 hd2 then
-      compare_int_lists tl1 tl2
-    else
-      false
-  | _ -> failwith "We expected lists."
-
-let compare_lists l1 l2 =
-  match l1, l2 with
-  | hd1::_, hd2::_ ->
-    (
-      match hd1, hd2 with
-      | TmConst(_,CInt(_)), TmConst(_,CInt(_)) ->
-        compare_int_lists l1 l2
-      | _ -> failwith "Lists of this type is not allowed."
-    )
-  | _ -> failwith "We expected two lists."
-
-let compare_tm_lists tm_l1 tm_l2 =
-  match tm_l1, tm_l2 with
-  | TmList(l1), TmList(l2) ->
-    compare_lists l1 l2
-
-let rec compare_term_lists l1 l2 =
-  let rec compare_terms t1 t2 =
-    (match t1, t2 with
-     | TmChar(_,n1),TmChar(_,n2) -> n1 = n2
-     | TmConst(_,c1),TmConst(_,c2) -> c1 = c2
-     | TmNop, TmNop -> true
-     | TmVar(_,id1,_,_), TmVar(_,id2,_,_) -> id1 = id2
-     | TmLam(_,var1,ty1,tm1), TmLam(_,var2,ty2,tm2) ->
-       (var1=var2) && (ty1=ty2) && (compare_terms tm1 tm2)
-     | TmClos(_,var1,ty1,tm1,_,_), TmClos(_,var2,ty2,tm2,_,_) ->
-       (var1=var2) && (ty1=ty2) && (compare_terms tm1 tm2)
-     | TmApp(_,tm11,tm12), TmApp(_,tm21,tm22) ->
-       (compare_terms tm11 tm21) && (compare_terms tm12 tm22)
-     | TmIfexp(_,tm11,tm12,tm13), TmIfexp(_,tm21,tm22,tm23) ->
-       (compare_terms tm11 tm21) && (compare_terms tm12 tm22) && (compare_terms tm13 tm23)
-     | TmFix _, TmFix _ -> true
-     | TmSeq(_,_,tm_l1,seq1,ds_choice1), TmSeq(_,_,tm_l2,seq2,ds_choice2) ->
-       (compare_term_lists (get_list_from_tmlist tm_l1) (get_list_from_tmlist tm_l2)) && (compare_sequences seq1 seq2)
-     | TmSeqMethod(_,fun_name1,_,_,_,_,_), TmSeqMethod(_,fun_name2,_,_,_,_,_) ->
-       fun_name1 = fun_name2
-     | _ -> false) in
-  match l1, l2 with
-  | [], [] -> true
-  | hd1::tl1, hd2::tl2 ->
-    if compare_terms hd1 hd2 then
-      compare_term_lists tl1 tl2
-    else
-      false
-  | _ -> false
-
-let rec split_pair_list l lhs_l rhs_l=
-  match l with
-  | [] -> (lhs_l,rhs_l)
-  | (hd1,hd2)::tl ->
-    let lhs_l' = hd1::lhs_l in
-    let rhs_l' = hd2::rhs_l in
-    split_pair_list tl lhs_l' rhs_l'
-
-let compare_term_pair_lists l1 l2 =
-  let (l11,l12) = split_pair_list l1 [] [] in
-  let (l21,l22) = split_pair_list l2 [] [] in
-  (compare_term_lists l11 l21) && (compare_term_lists l12 l22)
+let get_list_from_seqlist seql =
+  match seql with
+  | SeqList(ll) ->
+    Linkedlist.to_list ll
+  | SeqQueue(q) ->
+    Queue.to_list q
+  | SeqRTQueue(rtq) ->
+    Realtimequeue.to_list rtq
+  | _ -> failwith "Sequence type not implemented"
 
 (* Check if two value terms are equal *)
 let rec val_equal v1 v2 =
@@ -228,7 +144,9 @@ let rec val_equal v1 v2 =
       in o1 = o2 && u1 = u2 && eql (uct2revlist t1) (uct2revlist t2)
   | TmNop,TmNop -> true
   | TmSeq(_,_,_,seq1,ds_choice1), TmSeq(_,_,_,seq2,ds_choice2) ->
-    compare_sequences seq1 seq2
+    let l1 = get_list_from_seqlist seq1 in
+    let l2 = get_list_from_seqlist seq2 in
+    compare_term_lists l1 l2
   | _ -> false
 
 let ustring2uctstring s =
---
+++ src/boot/comparers.ml
@@ -0,0 +1,100 @@
+open Ast
+open Linkedlist
+open Queue
+open Realtimequeue
+
+let compare_tm_terms tm1 tm2 =
+  match tm1, tm2 with
+  | TmConst(_,CInt(n1)), TmConst(_,CInt(n2)) -> (n1 = n2)
+  | _ -> false
+
+let rec compare_linked_lists ll1 ll2 i =
+  if (i = Linkedlist.length ll1) && (i = Linkedlist.length ll2) then
+    true
+  else if (i = Linkedlist.length ll1) || (i = Linkedlist.length ll2) then
+    false
+  else if (compare_tm_terms (Linkedlist.nth ll1 i) (Linkedlist.nth ll2 i)) then
+    compare_linked_lists ll1 ll2 (i+1)
+  else
+    false
+
+let rec compare_int_lists l1 l2 =
+  match l1, l2 with
+  | [], [] -> true
+  | hd1::tl1, hd2::tl2 ->
+    if compare_tm_terms hd1 hd2 then
+      compare_int_lists tl1 tl2
+    else
+      false
+  | _ -> failwith "We expected lists."
+
+let compare_lists l1 l2 =
+  match l1, l2 with
+  | hd1::_, hd2::_ ->
+    (
+      match hd1, hd2 with
+      | TmConst(_,CInt(_)), TmConst(_,CInt(_)) ->
+        compare_int_lists l1 l2
+      | _ -> failwith "Lists of this type is not allowed."
+    )
+  | _ -> failwith "We expected two lists."
+
+let compare_tm_lists tm_l1 tm_l2 =
+  match tm_l1, tm_l2 with
+  | TmList(l1), TmList(l2) ->
+    compare_lists l1 l2
+
+let rec compare_term_lists l1 l2 =
+  let compare_sequences seq1 seq2 =
+    (let (l1,l2) =
+       (match seq1, seq2 with
+        | SeqList(ll1), SeqList(ll2) ->
+          ((Linkedlist.to_list ll1), (Linkedlist.to_list ll2))
+        | SeqQueue(q1), SeqQueue(q2) ->
+          ((Queue.to_list q1), (Queue.to_list q2))
+        | SeqRTQueue(rtq1), SeqRTQueue(rtq2) ->
+          ((Realtimequeue.to_list rtq1), (Realtimequeue.to_list rtq2))
+        | SeqNone, SeqNone -> ([], [])
+        | _ -> failwith "Comparison of sequence type not implemented.") in
+     compare_term_lists l1 l2) in
+  let rec compare_terms t1 t2 =
+    (match t1, t2 with
+     | TmChar(_,n1),TmChar(_,n2) -> n1 = n2
+     | TmConst(_,c1),TmConst(_,c2) -> c1 = c2
+     | TmNop, TmNop -> true
+     | TmVar(_,id1,_,_), TmVar(_,id2,_,_) -> id1 = id2
+     | TmLam(_,var1,ty1,tm1), TmLam(_,var2,ty2,tm2) ->
+       (var1=var2) && (ty1=ty2) && (compare_terms tm1 tm2)
+     | TmClos(_,var1,ty1,tm1,_,_), TmClos(_,var2,ty2,tm2,_,_) ->
+       (var1=var2) && (ty1=ty2) && (compare_terms tm1 tm2)
+     | TmApp(_,tm11,tm12), TmApp(_,tm21,tm22) ->
+       (compare_terms tm11 tm21) && (compare_terms tm12 tm22)
+     | TmIfexp(_,tm11,tm12,tm13), TmIfexp(_,tm21,tm22,tm23) ->
+       (compare_terms tm11 tm21) && (compare_terms tm12 tm22) && (compare_terms tm13 tm23)
+     | TmFix _, TmFix _ -> true
+     | TmSeq(_,_,tm_l1,seq1,ds_choice1), TmSeq(_,_,tm_l2,seq2,ds_choice2) ->
+       (compare_term_lists (get_list_from_tmlist tm_l1) (get_list_from_tmlist tm_l2)) && (compare_sequences seq1 seq2)
+     | TmSeqMethod(_,fun_name1,_,_,_,_,_), TmSeqMethod(_,fun_name2,_,_,_,_,_) ->
+       fun_name1 = fun_name2
+     | _ -> false) in
+  match l1, l2 with
+  | [], [] -> true
+  | hd1::tl1, hd2::tl2 ->
+    if compare_terms hd1 hd2 then
+      compare_term_lists tl1 tl2
+    else
+      false
+  | _ -> false
+
+let rec split_pair_list l lhs_l rhs_l=
+  match l with
+  | [] -> (lhs_l,rhs_l)
+  | (hd1,hd2)::tl ->
+    let lhs_l' = hd1::lhs_l in
+    let rhs_l' = hd2::rhs_l in
+    split_pair_list tl lhs_l' rhs_l'
+
+let compare_term_pair_lists l1 l2 =
+  let (l11,l12) = split_pair_list l1 [] [] in
+  let (l21,l22) = split_pair_list l2 [] [] in
+  (compare_term_lists l11 l21) && (compare_term_lists l12 l22)
